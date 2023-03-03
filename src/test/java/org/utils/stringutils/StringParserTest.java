9ec1cf200e55de40e033e8d050648b20e1871467
Zijin <e0772699@u.nus.edu>
2023-03-03 20:51:47 +0800

    Update code following lecture instructions

---
+++ src/test/java/org/utils/stringutils/StringParserTest.java
@@ -0,0 +1,30 @@
+package org.utils.stringutils;
+
+import org.junit.jupiter.api.Test;
+import org.utils.exceptions.InvalidStringInputException;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringParserTest {
+
+    //methodUnderTest_input_expected
+    @Test
+    void splitString_oneSep_expectTwoParts() throws InvalidStringInputException {
+        String inputString = "Hello|World";
+        String separator = "\\|";
+        StringParser sp = new StringParser();
+        ArrayList<String> stringParts = sp.splitString(inputString,separator);
+        assertEquals(2, stringParts.size());
+    }
+    @Test
+    void splitString_nullInput_expectException() {
+        String inputString = null;
+        String separator = "\\|";
+        StringParser sp = new StringParser();
+        //ArrayList<String> stringParts = sp.splitString(inputString,separator);
+        assertThrows(InvalidStringInputException.class,
+                ()->sp.splitString(inputString, separator));
+    }
+}
